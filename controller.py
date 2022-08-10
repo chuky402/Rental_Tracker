17bb76eb9011f4a8a2ec5570d7a446ff4c6a66fa
Chandler Brown <Chanman40025@gmail.com>
2022-08-10 17:02:40 -0500

    Starting

--- controller.py
+++ controller.py
@@ -1,8 +1,13 @@
 from PyQt5.QtWidgets import *
 from view import Ui_MainWindow
+import csv
 
 
 class Controller(QMainWindow, Ui_MainWindow):
-    def __int__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
+
+    def add(self):
+        name = self.name_input.text()
+
