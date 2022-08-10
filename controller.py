27019e73e1e2ad315aab1c15f8208ca164876892
Chandler Brown <Chanman40025@gmail.com>
2022-08-10 18:36:32 -0500

    Added a functional Gui along with the add button grabbing the information and storing it to variables.

--- controller.py
+++ controller.py
@@ -8,6 +8,15 @@ class Controller(QMainWindow, Ui_MainWindow):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
 
-    def add(self):
-        name = self.name_input.text()
+        self.button_add.clicked.connect(lambda: self.add())
 
+    def add(self):
+        address = self.input_address.text()
+        tenants = self.input_tenant.text()
+        if self.option_pet_yes.isChecked():
+            pet = 'yes'
+        elif self.option_pet_no.isChecked():
+            pet = 'no'
+        rent = self.combo_rent.currentText()
+        output = f'{tenants} live at {address}, Do pets live here, {pet} and their rent is {rent}'
+        print(output)
