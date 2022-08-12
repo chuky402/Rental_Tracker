dbb6fbd1859a161fd1024304bcefb6cc4c0d9df0
Chandler Brown <Chanman40025@gmail.com>
2022-08-12 03:53:29 -0500

    Commit

--- controller.py
+++ controller.py
@@ -52,7 +52,6 @@ class Controller(QMainWindow, Ui_MainWindow):
         number_id = self.id_number.text()
         search = self.id_number.text()
         result = str(db.search(Query().ID == search))
-        print(result[1:-1] + 'result')
         db.remove(where('ID') == number_id)
 
         self.insert(address, tenants, pet, rent, note, number_id)
@@ -64,26 +63,44 @@ class Controller(QMainWindow, Ui_MainWindow):
         """
         search = self.id_number.text()
         result = str(db.search(Query().ID == search))
-        print(result[1:-1] + 'result')
-        data = result[1:-1]
-        print(data)
-        res = ast.literal_eval(data)
-        print(str(res) + 'res')
-        rent = res['rent']
-        address = res['address']
-        tenants = res['Tenants']
-        pet = res['pet']
-        notes = res['notes']
-        number_id = res['ID']
-
-        self.id_number.setValue(0 if number_id is None else int(number_id))
-        self.combo_rent.setCurrentText('$0/Month' if rent is None else rent)
-        self.input_address.setText('No Address listed' if address is None else address)
-        self.number_tenants.setValue(0 if tenants is None else int(tenants))
-        if pet == 'yes':
-            self.option_pet_yes.setChecked(True)
-        elif pet == 'no':
-            self.option_pet_no.setChecked(True)
+        print(result)
+        if result != '[]':
+            data = result[1:-1]
+            res = ast.literal_eval(data)
+            rent = res['rent']
+            address = res['address']
+            tenants = res['Tenants']
+            pet = res['pet']
+            notes = res['notes']
+            number_id = res['ID']
+            self.id_number.setValue(int(number_id))
+            self.combo_rent.setCurrentText(rent)
+            self.input_address.setText(address)
+            self.number_tenants.setValue(int(tenants))
+            if pet == 'yes':
+                self.option_pet_yes.setChecked(True)
+            elif pet == 'no':
+                self.option_pet_no.setChecked(True)
+            else:
+                self.option_pet_no.setChecked(False)
+            self.tenant_notes.setText(notes)
         else:
-            self.option_pet_no.setChecked(False)
-        self.tenant_notes.setText('Invalid Tenant ID Number' if notes is None else notes)
+            rent = '$0/Month'
+            number_id = self.id_number.text()
+            address = ''
+            tenants = 0
+            pet = ''
+            notes = 'Invalid Tenant ID'
+
+            self.id_number.setValue(int(number_id))
+            self.combo_rent.setCurrentText(rent)
+            self.input_address.setText(address)
+            self.number_tenants.setValue(int(tenants))
+            if pet == 'yes':
+                self.option_pet_yes.setChecked(True)
+            elif pet == 'no':
+                self.option_pet_no.setChecked(True)
+            else:
+                self.option_pet_no.setChecked(False)
+                self.option_pet_yes.setChecked(False)
+            self.tenant_notes.setText(notes)
